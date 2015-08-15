e6f5bc16fe8a9e96908e8e38f4aad4fdbdeae4e6
Wang Lei <lane128@gmail.com>
2015-08-15 11:07:45 +0800

    Initial Commit

---
+++ AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by Lane on 8/15/15.
+//  Copyright (c) 2015 Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
